8522f752f34afbd12ed7ea0bd9db4e3b2fafbf3d
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-02-15 00:36:06 +0700

    Use locale th in th date pipe

--- src/app/pipes/th-date.pipe.ts
+++ src/app/pipes/th-date.pipe.ts
@@ -1,24 +1,30 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { DatePipe } from '@angular/common';
+import { Pipe, PipeTransform, Inject } from '@angular/core';
+import { DatePipe, registerLocaleData } from '@angular/common';
+import localeTh from '@angular/common/locales/th';
+import localeThExtra from '@angular/common/locales/extra/th';
 
 @Pipe({
   name: 'thDate'
 })
 export class ThDatePipe implements PipeTransform {
-
-  transform(value: any, format: string = 'd MMMM T tttt', timezone: string = '+0700', locale: string = 'th'): any {
+  transform(value: any, format: string = 'd MMMM yyyy', timezone: string = '+0700'): any {
     if (!value) {
       return '';
     }
-    const datePipe = new DatePipe(locale);
-    const thYear = (parseInt(datePipe.transform(value, 'yyyy', timezone), 10) + 543).toString();
+    registerLocaleData(localeTh, 'th', localeThExtra);
+    format = 'd MMMM GG tttt';
+
+    const datePipe = new DatePipe('th');
+    const buddhistEraYear = (parseInt(datePipe.transform(value, 'yyyy', timezone), 10) + 543).toString();
     const extraMap = {
-      'tttt': thYear.padStart(4, '0'),
-      'ttt': thYear.padStart(3, '0'),
-      'tt': thYear.padStart(2, '0'),
-      't': thYear[0],
-      'T': 'พ.ศ.'
+      'tttt': buddhistEraYear.padStart(4, '0'),
+      'ttt': buddhistEraYear.padStart(3, '0'),
+      'tt': buddhistEraYear.padStart(2, '0'),
+      't': buddhistEraYear,
+      'ค.ศ.': 'พ.ศ.',
+      'คริสตศักราช': 'พุทธศักราช'
     };
+
     let result = datePipe.transform(value, format, timezone);
     for (const [k, v] of Object.entries(extraMap)) {
       result = result.replace(k, v);
